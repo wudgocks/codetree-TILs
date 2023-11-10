d9e4c0c526cf742d9e4fab4e3074f44ff6ad87c7
wudgocks <115064755+wudgocks@users.noreply.github.com>
2023-11-10 18:22:32 +0900

    Title: 체크 다이아몬드, Time: 148ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 231110/체크 다이아몬드/check-diamond.py
@@ -0,0 +1,17 @@
+n = int(input())
+
+for i in range(n) :
+    for j in range(n - 1 - i) :
+        print(" ", end = "")
+    for j in range(i + 1) :
+        print("* ", end ="")
+    print()
+
+for i in range(n) :
+    for j in range(i+ 1) :
+        print(" ", end = "")
+
+    for j in range(n-i-1) :
+        print("* ", end ="")
+    
+    print()
