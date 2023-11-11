0f818723d7d4e46e4dc300af5d3320fece67321b
wudgocks <115064755+wudgocks@users.noreply.github.com>
2023-11-11 22:12:41 +0900

    Title: 정상적인 구구단, Time: 109ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 231111/정상적인 구구단/normal-multiple.py
@@ -0,0 +1,9 @@
+n = int(input())
+
+for i in range(1,n+1) :
+    for j in range(1, n + 1) :
+        if j == n :
+            print(f'{i} * {j} = {i*j} ', end = " ")
+        else :
+            print(f'{i} * {j} = {i*j},', end = " ")
+    print()
