76c1216abb2625d04afe02ecd44047667ee43cd4
wudgocks <115064755+wudgocks@users.noreply.github.com>
2023-11-11 22:29:19 +0900

    Title: 구구단 만들기, Time: 112ms, Memory: 24MB, Status: Passed - CodeTree

--- 231111/구구단 만들기/print-multiplication-table.py
+++ 231111/구구단 만들기/print-multiplication-table.py
@@ -1,10 +1,10 @@
 n,m = map(int,input().split())
 num = m
 for i in range(1, 9+1) :
-    for j in range(m // n) :
+    for j in range(m, n-1, -2) :
         print(f"{num} * {i} = {num*i} " , end = "")
         num -= 2
-        if j != m//n -1:
+        if j != n:
             print("/", end=" ")
     num = m
     print()
