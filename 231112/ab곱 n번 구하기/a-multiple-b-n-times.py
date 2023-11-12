1e20377eec085d6ee4b30ecea0c2432e5f6db78e
wudgocks <115064755+wudgocks@users.noreply.github.com>
2023-11-12 16:07:42 +0900

    Title: ab곱 n번 구하기, Time: 118ms, Memory: 24MB, Status: Passed - CodeTree

--- 231112/ab곱 n번 구하기/a-multiple-b-n-times.py
+++ 231112/ab곱 n번 구하기/a-multiple-b-n-times.py
@@ -1,8 +1,23 @@
-n = int(input())
+''' 입력과 즉시에 출력 '''
+# n = int(input())
 
+# for i in range(n) :
+#     a, b = map(int,input().split())
+#     tmp = 1
+#     for j in range(a, b + 1) :
+#         tmp *= j
+#     print(tmp)
+
+
+'''입력을 배열에 저장해서 한번에 출력 '''
+n = int(input())
+answer_lst = []
 for i in range(n) :
     a, b = map(int,input().split())
     tmp = 1
     for j in range(a, b + 1) :
         tmp *= j
-    print(tmp)
+    answer_lst.append(tmp)
+    
+for i in range(len(answer_lst)) :
+    print(answer_lst[i])
