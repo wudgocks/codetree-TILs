0d05e6d8b3dbaacbd79580561685a881214373d8
wudgocks <115064755+wudgocks@users.noreply.github.com>
2023-11-12 15:27:44 +0900

    Title: 알파벳 출력하기, Time: 112ms, Memory: 24MB, Status: Passed - CodeTree

--- 231112/알파벳 출력하기/print-alphabet.py
+++ 231112/알파벳 출력하기/print-alphabet.py
@@ -3,6 +3,8 @@ n = int(input())
 target_chr = 'A'
 for i in range(n) :
     for j in range(i + 1) :
+        if ord(target_chr) == ord('Z') + 1 :
+            target_chr = 'A'
         print(target_chr, end = "")
         target_chr = chr(ord(target_chr) + 1)
     print()
