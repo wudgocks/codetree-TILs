afb9a2763d06e7ec4458f9c355a6bf5d9c780b14
wudgocks <115064755+wudgocks@users.noreply.github.com>
2023-11-12 15:40:34 +0900

    Title: 알파벳 출력하기 2, Time: 129ms, Memory: 24MB, Status: Passed - CodeTree

--- 231112/알파벳 출력하기 2/print-alphabet-2.py
+++ 231112/알파벳 출력하기 2/print-alphabet-2.py
@@ -4,6 +4,8 @@ for i in range(n) :
     for j in range(i) :
         print("  ", end = "")
     for j in range(n-i) :
+        if ord(target) == ord('Z') + 1 :
+            target = 'A'
         print(target, end = " ")
         target = chr(ord(target) + 1)
     print()
