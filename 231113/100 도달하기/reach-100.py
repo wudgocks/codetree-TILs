e3483fa7834a165aaa5926503075846014bdf699
wudgocks <115064755+wudgocks@users.noreply.github.com>
2023-11-13 15:45:28 +0900

    Title: 100 도달하기, Time: 112ms, Memory: 27MB, Status: Passed - CodeTree

--- 231113/100 도달하기/reach-100.py
+++ 231113/100 도달하기/reach-100.py
@@ -1,10 +1,10 @@
 n = int(input())
 lst = [1, n]
 
-for i in range(3,100) :
+while True :
     number = lst[-1]+ lst[-2]
     lst.append(number)
-    if number >= 100 :
+    if number > 100 :
         break
 
 for element in lst :
