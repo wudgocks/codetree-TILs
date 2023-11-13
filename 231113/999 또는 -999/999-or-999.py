0afedc12c1198c0ff884dd81454a84a88f0623ac
wudgocks <115064755+wudgocks@users.noreply.github.com>
2023-11-13 19:44:04 +0900

    Title: 999 또는 -999, Time: 112ms, Memory: 24MB, Status: Passed - CodeTree

--- 231113/999 또는 -999/999-or-999.py
+++ 231113/999 또는 -999/999-or-999.py
@@ -6,6 +6,8 @@ minVal = sys.maxsize
 lst = list(map(int,input().split()))
 
 for element in lst :
+    if element == 999 or element == -999 :
+        break
     if element != 999 and element > maxVal :
         maxVal = element
     if element != - 999 and element < minVal :
