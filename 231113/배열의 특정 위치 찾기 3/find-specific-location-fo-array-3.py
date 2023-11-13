a8a2d9991112f54eac1094d856e361684e802080
wudgocks <115064755+wudgocks@users.noreply.github.com>
2023-11-13 14:33:43 +0900

    Title: 배열의 특정 위치 찾기 3, Time: 116ms, Memory: 24MB, Status: Passed - CodeTree

--- 231113/배열의 특정 위치 찾기 3/find-specific-location-fo-array-3.py
+++ 231113/배열의 특정 위치 찾기 3/find-specific-location-fo-array-3.py
@@ -1,14 +1,26 @@
-lst = list(map(int,input().split()))
-new_arr = []
+''' 새로운 리스트 만들어서 뒤의 세개 구하기 '''
+# lst = list(map(int,input().split()))
+# new_arr = []
 
-for i in range(len(lst)) :
-    if lst[i] == 0 :
+# for i in range(len(lst)) :
+#     if lst[i] == 0 :
+#         break
+#     new_arr.append(lst[i])
+
+# sumVal = 0
+
+# for i in range(len(new_arr)-1, len(new_arr)-4, -1) :
+#     sumVal += new_arr[i]
+
+# print(sumVal)
+
+''' arr[k-3], [k-2], [k-1] 구하기 '''
+
+arr = list(map(int,input().split()))
+
+for i in range (len(arr)) :
+    if arr[i] == 0 :
+        k = i # 0 이 나온 인덱스 구하기
         break
-    new_arr.append(lst[i])
 
-sumVal = 0
-
-for i in range(len(new_arr)-1, len(new_arr)-4, -1) :
-    sumVal += new_arr[i]
-
-print(sumVal)
+print(arr[k-3] + arr[k-2] + arr[k-1])
