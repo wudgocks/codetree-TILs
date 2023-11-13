3d680fb120549d27d6577312422f20e76f8720a7
wudgocks <115064755+wudgocks@users.noreply.github.com>
2023-11-13 15:16:12 +0900

    Title: 짝수인 것만 출력하기, Time: 112ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 231113/짝수인 것만 출력하기/print-even-number.py
+++ 231113/짝수인 것만 출력하기/print-even-number.py
@@ -2,6 +2,12 @@ n = int(input())
 
 lst = list(map(int,input().split()))
 
+new_lst = []
+
+
 for i in range(len(lst)) :
     if lst[i] % 2 == 0 :
-        print(lst[i], end = " ")
+        new_lst.append(lst[i])
+
+for i in range(len(new_lst)) :
+    print(new_lst[i])
