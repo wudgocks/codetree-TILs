c8a01c780de00e431726ada42694db2732d8948e
wudgocks <115064755+wudgocks@users.noreply.github.com>
2023-11-14 14:25:05 +0900

    Title: 중복되지 않는 숫자 중 최대, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 231114/중복되지 않는 숫자 중 최대/max-of-unique-number.py
@@ -0,0 +1,26 @@
+import sys
+
+n = int(input())
+
+lst = list(map(int,input().split()))
+counting = [0] * 1000
+
+newLst = []
+
+for element in lst :
+    counting[element] += 1
+
+for i in range(len(counting)) :
+    if counting[i] == 1 :
+        newLst.append(i)
+
+maxVal = -sys.maxsize
+
+for element in newLst :
+    if element > maxVal :
+        maxVal = element
+
+if maxVal == -sys.maxsize :
+    print(-1)
+else :
+    print(maxVal)
