b58d53f2135e786acca742dd2cdb39a7f521c5e0
wudgocks <115064755+wudgocks@users.noreply.github.com>
2023-11-14 17:34:44 +0900

    Title: 두 개의 격자 비교하기, Time: 143ms, Memory: 24MB, Status: Passed - CodeTree

--- 231114/두 개의 격자 비교하기/compare-two-grid.py
+++ 231114/두 개의 격자 비교하기/compare-two-grid.py
@@ -3,12 +3,12 @@ n,m = map(int,input().split())
 # 배열 두개 입력
 arr1 = [
     list(map(int,input().split()))
-    for _ in range(m) 
+    for _ in range(n) 
 ]
 
 arr2 = [
     list(map(int,input().split()))
-    for _ in range(m) 
+    for _ in range(n) 
 ]
 
 # 결과를 출력할 배열 선언
