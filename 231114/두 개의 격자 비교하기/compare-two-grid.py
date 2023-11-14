ccab341f93a23a9012097f69536db3126f1267f3
wudgocks <115064755+wudgocks@users.noreply.github.com>
2023-11-14 17:34:02 +0900

    Title: 두 개의 격자 비교하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 231114/두 개의 격자 비교하기/compare-two-grid.py
+++ 231114/두 개의 격자 비교하기/compare-two-grid.py
@@ -1,8 +1,29 @@
-n,m = map(int(input().split()))
+n,m = map(int,input().split())
 
+# 배열 두개 입력
 arr1 = [
     list(map(int,input().split()))
-    for _ in range(3) 
+    for _ in range(m) 
 ]
 
-print(arr1)
+arr2 = [
+    list(map(int,input().split()))
+    for _ in range(m) 
+]
+
+# 결과를 출력할 배열 선언
+arr_result = [
+    [1 for _ in range(m)]
+    for _ in range(n)
+]
+
+
+for i in range(n) :
+    for j in range(m) :
+        if arr1[i][j] == arr2[i][j] :
+            arr_result[i][j] = 0
+
+for i in range(n) :
+    for j in range(m) :
+        print(arr_result[i][j], end = " ")
+    print()
