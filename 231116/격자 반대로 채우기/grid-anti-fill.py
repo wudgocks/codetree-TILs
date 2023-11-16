8a8a30e9f2cc3e5474e38c9727dc21aeed01a2ac
wudgocks <115064755+wudgocks@users.noreply.github.com>
2023-11-16 11:51:19 +0900

    Title: 격자 반대로 채우기, Time: 116ms, Memory: 24MB, Status: Passed - CodeTree

--- 231116/격자 반대로 채우기/grid-anti-fill.py
+++ 231116/격자 반대로 채우기/grid-anti-fill.py
@@ -6,17 +6,32 @@ arr_2d = [
 ]
 
 number = 1
+cheker = True
 
-for i in range(n-1,-1,-1) :
-    if i%2 == 1 :
-        for j in range(n-1,-1,-1) :
-            arr_2d[j][i] = number
-            number += 1
-    else :
-        for j in range(n) :
-            arr_2d[j][i] = number 
-            number += 1
+# 홀수일때는 다르게 처리
+if n % 2 == 1 :
+    cheker = False
 
+if cheker :
+    for i in range(n-1,-1,-1) :
+        if i%2 == 1 :
+            for j in range(n-1,-1,-1) :
+                arr_2d[j][i] = number
+                number += 1
+        else :
+            for j in range(n) :
+                arr_2d[j][i] = number 
+                number += 1
+else :
+    for i in range(n-1,-1,-1) :
+        if i%2 == 0 :
+            for j in range(n-1,-1,-1) :
+                arr_2d[j][i] = number
+                number += 1
+        else :
+            for j in range(n) :
+                arr_2d[j][i] = number 
+                number += 1
 
 for i in range(n) :
     for j in range(n) :
