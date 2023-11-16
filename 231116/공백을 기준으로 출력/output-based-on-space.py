082dcca0a475eb9ed71c9d62d97adde2968ebf09
wudgocks <115064755+wudgocks@users.noreply.github.com>
2023-11-16 13:35:50 +0900

    Title: 공백을 기준으로 출력, Time: 105ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 231116/공백을 기준으로 출력/output-based-on-space.py
@@ -0,0 +1,10 @@
+s1 = input()
+s2 = input()
+
+for i in range(len(s1)) :
+    if s1[i] != " " :
+        print(s1[i], end= "" )
+
+for i in range(len(s2)) :
+    if s2[i] != " " :
+        print(s2[i], end= "" )
