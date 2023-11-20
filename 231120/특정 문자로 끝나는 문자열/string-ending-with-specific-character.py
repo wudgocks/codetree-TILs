af25571680df977aed82885e1e3af78299eed18f
wudgocks <115064755+wudgocks@users.noreply.github.com>
2023-11-20 13:27:21 +0900

    Title: 특정 문자로 끝나는 문자열, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 231120/특정 문자로 끝나는 문자열/string-ending-with-specific-character.py
@@ -0,0 +1,14 @@
+lst = []
+for _ in range(10) :
+    string = input()
+    lst.append(string)
+
+one = input()
+checker = 0
+for string in lst :
+    if string[len(string)-1] == one :
+        print(string)
+        counter = 1
+
+if checker == 0 :
+    print('None')
