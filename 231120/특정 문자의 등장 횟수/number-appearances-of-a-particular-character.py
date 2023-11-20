013dbbc9251eda4890b00c364c8019124b11b023
wudgocks <115064755+wudgocks@users.noreply.github.com>
2023-11-20 15:08:28 +0900

    Title: 특정 문자의 등장 횟수, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 231120/특정 문자의 등장 횟수/number-appearances-of-a-particular-character.py
+++ 231120/특정 문자의 등장 횟수/number-appearances-of-a-particular-character.py
@@ -2,9 +2,11 @@ string = input()
 
 cnt1 = 0
 cnt2 = 0
-if 'ee' in string :
-    cnt1 += 1
 
-if 'eb' in string :
-    cnt2 += 1
+for i in range(len(string)-1) :
+    if string[i] == 'e' and string[i+ 1] =='e' :
+        cnt1 += 1
+    if string[i] == 'e' anmd stringp[i+1] =='b' :
+        cnt2 += 1
+
 print(cnt1, cnt2)
