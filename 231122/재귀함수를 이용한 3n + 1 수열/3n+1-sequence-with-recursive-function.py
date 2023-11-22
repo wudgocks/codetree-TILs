f7b891c06602d9837100f75db1d9358feeb7c4f2
wudgocks <115064755+wudgocks@users.noreply.github.com>
2023-11-22 17:41:41 +0900

    Title: 재귀함수를 이용한 3n + 1 수열, Time: 119ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 231122/재귀함수를 이용한 3n + 1 수열/3n+1-sequence-with-recursive-function.py
@@ -0,0 +1,12 @@
+n = int(input())
+
+def function (n) : 
+    if n == 1 :
+        return 0
+
+    if n % 2 == 0 :
+        return function(n // 2) + 1
+    else :
+        return function(3* n + 1) + 1
+
+print(function(n))
