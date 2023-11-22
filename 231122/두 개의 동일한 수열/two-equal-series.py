2a4114007e59d20f0beccc88b699a02f936ec00f
wudgocks <115064755+wudgocks@users.noreply.github.com>
2023-11-22 18:08:36 +0900

    Title: 두 개의 동일한 수열, Time: 111ms, Memory: 24MB, Status: Passed - CodeTree

--- 231122/두 개의 동일한 수열/two-equal-series.py
+++ 231122/두 개의 동일한 수열/two-equal-series.py
@@ -3,18 +3,16 @@ n = int(input())
 A = list(map(int,input().split()))
 B = list(map(int,input().split()))
 
+def equal() :
+    for elem1, elem2 in zip(A,B) :
+        if elem1 != elem2 :
+            return False
+    return True
+
 A.sort()
 B.sort()
 
-checker = False 
-
-for i in range(n) :
-    if A[i] != B[i] :
-        checker = False
-    else :
-        checker = True
-
-if checker :
+if equal() :
     print('Yes')
 else :
     print('No')
