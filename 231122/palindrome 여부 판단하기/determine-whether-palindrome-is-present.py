d444383c5670f42af2376408ba7a4f0154d7fffa
wudgocks <115064755+wudgocks@users.noreply.github.com>
2023-11-22 13:20:16 +0900

    Title: palindrome 여부 판단하기, Time: 108ms, Memory: 24MB, Status: Passed - CodeTree

--- 231122/palindrome 여부 판단하기/determine-whether-palindrome-is-present.py
+++ 231122/palindrome 여부 판단하기/determine-whether-palindrome-is-present.py
@@ -5,10 +5,13 @@ def check_palindrome(string) :
     for i in range(len(string)-1, -1, -1) :
         reverstring += string[i]
     
+    string = ''.join(string)
+    
     if string == reverstring :
         print("Yes")
     else :
         print("No")
+    
     
 _str = input()
 
