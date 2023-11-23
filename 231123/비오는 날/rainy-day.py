abd4072f28811513f7203cf3f232bb86d83ef9f1
wudgocks <115064755+wudgocks@users.noreply.github.com>
2023-11-23 15:24:11 +0900

    Title: 비오는 날, Time: 107ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 231123/비오는 날/rainy-day.py
+++ 231123/비오는 날/rainy-day.py
@@ -12,7 +12,7 @@ for _ in range(n) :
     date, day, weather = tuple(input().split())
     forcast_info.append(Forcast(date,day, weather))
 
-arr = [w.weather for w in forcast_info]
+arr = [d.date for d in forcast_info]
 arr.sort()
 for i in range(n) :
     if forcast_info[i].weather == 'Rain' :
