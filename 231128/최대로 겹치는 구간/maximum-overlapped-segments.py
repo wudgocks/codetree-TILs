99f6e52b5ea8b8bd1133c51ac8a3e67cc9a4cf50
wudgocks <115064755+wudgocks@users.noreply.github.com>
2023-11-28 16:53:35 +0900

    Title: 최대로 겹치는 구간, Time: 116ms, Memory: 25MB, Status: Passed - CodeTree

--- 231128/최대로 겹치는 구간/maximum-overlapped-segments.py
+++ 231128/최대로 겹치는 구간/maximum-overlapped-segments.py
@@ -1,10 +1,12 @@
-# 구간의 경우에는 x1부터 x2 - 1까지 표시를 해주면 됩니다.
-n = int(input())
+OFFSET = 100
+MAX_R = 200
 
-checker = [0] * 200
+n = int(input())
+checker = [0] * (MAX_R + 1)
 
 for i in range(n) :
     x1,x2 = tuple(map(int,input().split()))
+    x1,x2 = x1 + OFFSET, x2 + OFFSET
     for x in range(x1,x2) :
         checker[x] += 1
 
