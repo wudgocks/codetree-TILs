1aa55dceee0c3d1492035daaa879649c85564a2c
wudgocks <115064755+wudgocks@users.noreply.github.com>
2023-12-01 15:34:01 +0900

    Title: k번째로 신기한 문자열, Time: 225ms, Memory: 24MB, Status: Passed - CodeTree

--- 231201/k번째로 신기한 문자열/kth-special-string.py
+++ 231201/k번째로 신기한 문자열/kth-special-string.py
@@ -5,9 +5,25 @@ k = int(k)
 wordLst = []
 T_leng = len(T)
 
+# 직접작성한 starts_with -- a가ㅠ로 시작하는지 확인
+def starts_with (a,b) :
+    if len(a) < len(b) :
+        return False
+    
+    # b의 길이만큼 살펴보며, a와 문자열이 동일한지 확인
+    return a[:len(b)] == b
+
+# 파이썬의 startswith 함수 사용
+'''
 for i in range(n) :
     string = input()
     if string.startswith(T) :
+        wordLst.append(string)
+'''
+
+for _ in range(n) :
+    string = input()
+    if starts_with(string, T) :
         wordLst.append(string)
 
 wordLst.sort()
