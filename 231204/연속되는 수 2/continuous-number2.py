7e31f529a16ae4d3d2cac1ab83b25419602df8ca
wudgocks <115064755+wudgocks@users.noreply.github.com>
2023-12-04 19:36:21 +0900

    Title: 연속되는 수 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 231204/연속되는 수 2/continuous-number2.py
@@ -0,0 +1,20 @@
+n = int(input())
+
+lst = []
+counter_lst = []
+
+for i in range(n) :
+    num = int(input())
+    lst.append(num)
+
+counter = 1
+
+for i in range(len(lst)) :
+    if lst[i] == lst[i-1] :
+        counter += 1
+    
+    elif i == 0 or lst[i] != lst[i-1] :
+        counter_lst.append(counter)
+        counter = 1
+
+print(max(counter_lst))
