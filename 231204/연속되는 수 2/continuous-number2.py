87494b0f4eb0cf400ca3928052e61c1da3956c8d
wudgocks <115064755+wudgocks@users.noreply.github.com>
2023-12-04 20:32:13 +0900

    Title: 연속되는 수 2, Time: 129ms, Memory: 25MB, Status: Passed - CodeTree

--- 231204/연속되는 수 2/continuous-number2.py
+++ 231204/연속되는 수 2/continuous-number2.py
@@ -10,12 +10,11 @@ for i in range(n) :
 
 counter = 1
 for i in range(len(lst)) :
-    # 값이 달라지면 checker = 1
-    if lst[i-1] == lst[i] :
-        counter += 1
+    if i ==0 or lst[i-1] != lst[i] :
+        counter = 1
         counterLst.append(counter)
     else :
-        counter = 1
+        counter += 1
         counterLst.append(counter)
         
 print(max(counterLst))
