85b45f68b45bef8d8f9983fd321716bea8950e2a
wudgocks <115064755+wudgocks@users.noreply.github.com>
2023-12-04 20:45:10 +0900

    Title: 연속되는 수 3, Time: 120ms, Memory: 24MB, Status: Passed - CodeTree

--- 231204/연속되는 수 3/continuous-number3.py
+++ 231204/연속되는 수 3/continuous-number3.py
@@ -6,17 +6,21 @@ lst = [
     for _ in range(n)
 ]
 
+counterLst = []
+
 for i in range(len(lst)) :
     if lst[i] >= 0 :
         lst[i] = 0
     else :
         lst[i] = 1
-minus = 0
-plus = 0
-for elem in lst :
-    if elem == 0 :
-        plus +=1
-    else :
-        minus += 1
 
-print(max(minus,plus))
+counter = 1
+for i in range(len(lst)) :
+    if i == 0 or lst[i] != lst[i-1] :
+        counter = 1
+        counterLst.append(counter)
+    else :
+        counter += 1
+        counterLst.append(counter)
+        
+print(max(counterLst))
