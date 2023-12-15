d5582c1fdfde280e336a607a3729ebe0f6264cac
wudgocks <115064755+wudgocks@users.noreply.github.com>
2023-12-15 17:04:05 +0900

    Title: 악수와 전염병의 상관관계 2, Time: 112ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 231215/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
+++ 231215/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
@@ -4,17 +4,16 @@ class Shake :
 
 
 # n 명의 개발자, k번의 악수로 전염, 처음 전염병에 걸린 번호 P, t번 악수
-n,k,p,T = map(int,input().split())
+n,k,p,t = map(int,input().split())
 shakes = []
-
-for _ in range(T) :
+for _ in range(t) :
     time,person1,person2 = tuple(map(int,input().split()))
     shakes.append(Shake(time,person1,person2))
 
 shake_num = [0] * (n + 1)
-inflected = [False] * (n + 1)
+infected = [False] * (n + 1)
 
-inflected[p] = True
+infected[p] = True
 
 shakes.sort(key = lambda x : x.time)
 
@@ -22,19 +21,19 @@ for shake in shakes :
     target1 = shake.person1
     target2 = shake.person2
 
-    if inflected[target1] :
+    if infected[target1] :
         shake_num[target1] += 1
-    if inflected[target2] :
+    if infected[target2] :
         shake_num[target2] += 2
     
-    if shake_num[target1] <= k and inflected[target1] :
-        inflected[target2] = True
+    if shake_num[target1] <= k and infected[target1] :
+        infected[target2] = True
     
-    if shake_num[target2] <= k and inflected[target2] :
-        inflected[target1] = True
+    if shake_num[target2] <= k and infected[target2] :
+        infected[target1] = True
 
 for i in range(1, n + 1) :
-    if inflected[i] :
+    if infected[i] :
         print(1, end="")
     else :
         print(0, end="")
