25b4d724803d6623cb56cbcb8d30ba2d39dc5d79
wudgocks <115064755+wudgocks@users.noreply.github.com>
2023-12-15 17:41:05 +0900

    Title: 1이 3개 이상 있는 위치, Time: 202ms, Memory: 26MB, Status: Passed - CodeTree

--- 231215/1이 3개 이상 있는 위치/place-more-than-3-ones.py
+++ 231215/1이 3개 이상 있는 위치/place-more-than-3-ones.py
@@ -1,28 +1,31 @@
 n = int(input())
 
-arr_2d = [
+arr = [
     list(map(int,input().split()))
     for _ in range(n)
 ]
-point = 0
-
-def in_range(x,y) :
-    return 0 <= x and x < n and 0 <= y and y < n
 
 dxs = [0,1,0,-1]
 dys = [1,0,-1,0]
 
-for row in range(n) :
-    for col in range(n) :
-        cnt = 0
-        for dx,dy in zip(dxs,dys) :
-            nx = row + dx
-            ny = col + dy
+# dx,dy에서 인접한 위치가 격자 안으로 들어오는지를 판단하는 in_range() 함수 작성
+def in_range(x,y) :
+    return 0 <= x and x < n and 0 <= y and y < n
 
-            if in_range(nx,ny) and arr_2d[nx][ny] == 1:
-                cnt += 1
-        
-        if cnt >= 3:
-            point += 1
+def adjacent_cnt (x,y) :
+    cnt = 0 
+    for dx,dy in zip(dxs,dys) :
+        nx,ny = x + dx, y + dy
+        if in_range(nx,ny) and arr[nx][ny] == 1 :
+            cnt += 1
+    return cnt 
 
-print(point)
+# 각 칸을 탐색
+ans = 0
+
+for i in range(n) :
+    for j in range(n) :
+        if adjacent_cnt(i,j) >= 3 :
+            ans += 1
+
+print(ans)
